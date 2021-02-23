97fde1c41ccf999491e86821313115034eccddf1
grodstrike <grodstrike@gmail.com>
2021-02-23 14:07:35 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Ошибка: Вы должны быть root, чтобы запустить этот скрипт. (Введите: sudo su)"
+    exit 1
+fi
+
+
 homedir=$HOME
+passdir='/root/bee-pass.txt'
+
+if [ ! -f $passdir ]; then
+	date "+【%Y-%m-%d %H:%M:%S】 Генерация /root/bee-pass.txt" 2>&1 | tee -a /root/run.log
+	echo "Введите пароль для ноды (он будет хранится тут $passdir):"
+	read  n
+	echo  $n > $passdir;
+	date "+【%Y-%m-%d %H:%M:%S】 Ваш пароль от ноды: " && cat $passdir  2>&1 | tee -a /root/run.log
+fi
 
 echo 'Установка пакетов'
 
+date "+【%Y-%m-%d %H:%M:%S】 Установка пакетов" 2>&1 | tee -a /root/run.log
 sudo apt-get update
-sudo apt -y install curl
-sudo apt -y install wget
-sudo apt -y install tmux
-sudo apt -y install jq
+sudo apt -y install curl wget tmux jq
 
 echo 'Установка Swarm Bee'
 
+date "+【%Y-%m-%d %H:%M:%S】 Установка Swarm Bee" 2>&1 | tee -a /root/run.log
 curl -s https://raw.githubusercontent.com/ethersphere/bee/master/install.sh | TAG=v0.5.0 bash
 
 echo 'Установка Bee Clef'
 
+date "+【%Y-%m-%d %H:%M:%S】 Установка Bee Clef" 2>&1 | tee -a /root/run.log
 wget https://github.com/ethersphere/bee-clef/releases/download/v0.4.7/bee-clef_0.4.7_amd64.deb && dpkg -i bee-clef_0.4.7_amd64.deb
-
-rm bee-clef_0.4.7_amd64.deb
 
 echo 'Создание конфига'
 
+date "+【%Y-%m-%d %H:%M:%S】 Создание конфига" 2>&1 | tee -a /root/run.log
 echo "api-addr: :1633
 bootnode:
 - /dnsaddr/bootnode.ethswarm.org
@@ -41,7 +57,7 @@ p2p-addr: :1634
 p2p-quic-enable: false
 p2p-ws-enable: false
 password: ""
-password-file: ""
+password-file: "/root/bee-pass.txt"
 payment-early: "1000000000000"
 payment-threshold: "10000000000000"
 payment-tolerance: "50000000000000"
@@ -57,18 +73,42 @@ tracing-service-name: bee
 verbosity: info
 welcome-message: ""
 " >> $homedir/bee-default.yaml
-echo 'Установка скрипта для обналичивания чеков'
-wget https://github.com/grodstrike/bee-swarm/raw/main/cashout.sh $homedir/cashout.sh && chmod a+x cashout.sh
 
+echo 'Установка скрипта для обналичивания чеков'
+date "+【%Y-%m-%d %H:%M:%S】 'Установка скрипта для обналичивания чеков" 2>&1 | tee -a /root/run.log
+wget https://github.com/grodstrike/bee-swarm/raw/main/cashout.sh && cp cashout.sh /root/cashout.sh
+sudo chmod 777 /root/cashout.sh
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-sudo echo "0 */6 * * * /bin/bash $homedir/cashout.sh cashout-all >> $homedir/cash.log   2>&1 " >> mycron
+echo "0 * * * * /bin/bash /root/cashout.sh cashout-all >> /root/cash.log >/dev/null 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
 sudo systemctl restart cron
-echo 'Запуск ноды'
-tmux new -d -s bee
-tmux send-keys -t bee.0 "sudo bee start --config $homedir/bee-default.yaml" ENTER
-tmux a -t bee
+
+date "+【%Y-%m-%d %H:%M:%S】 'Установка сервиса Swarm Bee" 2>&1 | tee -a /root/run.log
+echo "
+[Unit]
+Description=Bee Bzz Bzzzzz service
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=60
+User=root
+ExecStart=/usr/local/bin/bee start --config $homedir/bee-default.yaml
+[Install]
+WantedBy=multi-user.target
+" >> /etc/systemd/system/bee.service
+systemctl daemon-reload
+systemctl enable bee
+systemctl start bee
+
+echo ''
+echo -e "\e[42mУстановка завершена!\e[0m"; echo ''; echo 'Ваш пароль от ноды:' && cat $passdir && echo '' && echo 'Хранится по пути: '; echo $passdir
+echo 'Для активации ноды пополните токенами по инструкции https://telegra.ph/gbzz-geth-02-22'
+echo ''
+echo -e 'Запущена ли нода? Проверьте командой \e[42msystemctl status bee\e[0m'
+echo ''
