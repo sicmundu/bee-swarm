916230f0a18d098fd787fbcc11cc5999082624eb
grodstrike <grodstrike@gmail.com>
2021-02-24 10:52:04 +0400

    1.0.2

--- install-test.sh
+++ install-test.sh
@@ -1,16 +1,146 @@
 #!/bin/bash
-
+#
+# Скрипт установки Swarm Bee node
+#
 # Check if user is root
+#
+# thanks to root#2682
+
+echo "
++----------------------------------------------------------------------
+| Установка Swarm Bee  для CentOS/Ubuntu/Debian
++----------------------------------------------------------------------
+| Copyright © 2015-20021 https://t.me/ru_swarm All rights reserved.
++----------------------------------------------------------------------
+|
++----------------------------------------------------------------------
+";sleep 5
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=ru_RU.UTF-8
+cd ~
+
+# пути к осноаным файлам
+logPath='/root/bee-run.log'
+cashlogPath='/root/bee-cash.log'
+passPath='/root/bee-pass.txt'
+swapEndpoint='https://goerli.prylabs.net'
+cashScriptPath='/root/cashout.sh'
+
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$*${none}; }
+_green() { echo -e ${green}$*${none}; }
+_yellow() { echo -e ${yellow}$*${none}; }
+_magenta() { echo -e ${magenta}$*${none}; }
+_cyan() { echo -e ${cyan}$*${none}; }
+
 if [ $(id -u) != "0" ]; then
-    echo "Ошибка: Вы должны быть root, чтобы запустить этот скрипт. (Введите: sudo su)"
+    echo "Ошибка: Вы должны быть {red}root{none}, чтобы запустить этот скрипт. (Введите: sudo su)"
     exit 1
 fi
 
+createSwarmService(){
+    date "+【%Y-%m-%d %H:%M:%S】 Installing the Swarm Bee service" 2>&1 | tee -a $logPath
+	if [ ! -f /etc/systemd/system/bee.service ]; then
+	cat >> /etc/systemd/system/bee.service << EOE
+[Unit]
+Description=Bee Bzz Bzzzzz service
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=60
+User=root
+ExecStart=/usr/local/bin/bee start --config ${homedir}/bee-default.yaml
+[Install]
+WantedBy=multi-user.target
+EOE
+	echo 'Сервис уже установлен'
+	else date "+【%Y-%m-%d %H:%M:%S】 Сервис уже установлен" 2>&1 | tee -a $logPath
+fi
+
+systemctl daemon-reload
+systemctl enable bee
+systemctl start bee
+}
+
+getCashoutScript(){
+	if [ ! -f $cashScriptPath ]; then
+		date "+【%Y-%m-%d %H:%M:%S】 Installing a script for cashing checks" 2>&1 | tee -a $logPath
+	    wget -O $cashScriptPath https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/7ba05095e0836735f4a648aefe52c584e18e065f/cashout.sh && chmod a+x $cashScriptPath
+	else
+	    date "+【%Y-%m-%d %H:%M:%S】 '$cashScriptPath' Файл уже есть" 2>&1 | tee -a $logPath
+	fi
+
+	#write out current crontab
+	crontab -l > mycron
+	#echo new cron into cron file
+	echo "*/60 * * * * /bin/bash $cashScriptPath cashout-all >> $cashlogPath >/dev/null 2>&1" >> mycron
+	#install new cron file
+	crontab mycron
+	rm -f mycron
+	systemctl restart crond
+
+}
+
+
+createConfig(){
+	date "+【%Y-%m-%d %H:%M:%S】 Start Create Config" 2>&1 | tee -a $logPath
+	if [ ! -f $homedir/bee-default.yaml ]; then
+	cat >> $homedir/bee-default.yaml << EOF
+api-addr: :1633
+bootnode:
+- /dnsaddr/bootnode.ethswarm.org
+clef-signer-enable: false
+clef-signer-endpoint: ""
+config: /root/.bee.yaml
+cors-allowed-origins: []
+data-dir: /root/.bee
+db-capacity: "5000000"
+debug-api-addr: :1635
+debug-api-enable: true
+gateway-mode: false
+global-pinning-enable: false
+help: false
+nat-addr: ""
+network-id: "1"
+p2p-addr: :1634
+p2p-quic-enable: false
+p2p-ws-enable: false
+password: ""
+password-file: ${passPath}
+payment-early: "1000000000000"
+payment-threshold: "10000000000000"
+payment-tolerance: "50000000000000"
+resolver-options: []
+standalone: false
+swap-enable: true
+swap-endpoint: ${swapEndpoint}
+swap-factory-address: ""
+swap-initial-deposit: "100000000000000000"
+tracing-enable: false
+tracing-endpoint: 127.0.0.1:6831
+tracing-service-name: bee
+verbosity: 3
+welcome-message: ""
+EOF
+	else date "+【%Y-%m-%d %H:%M:%S】 Конфиг файл уже создан" 2>&1 | tee -a $logPath
+fi
+}
+
+
+
 
 homedir=$HOME
-passdir='/root/bee-pass.txt'
 
-if [ ! -f $passdir ]; then
+if [ ! -f $passPath ]; then
 	date "+【%Y-%m-%d %H:%M:%S】 Генерация /root/bee-pass.txt" 2>&1 | tee -a /root/run.log
 	echo "Введите пароль для ноды (он будет хранится тут $passdir):"
 	read  n
