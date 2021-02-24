5591e9459b459c78a4d36e4fce82c4634a32903a
grodstrike <grodstrike@gmail.com>
2021-02-24 16:15:49 +0400

    Update install-test.sh

--- install-test.sh
+++ install-test.sh
@@ -160,7 +160,7 @@ sudo apt -y install curl wget tmux jq
 
 echo 'Установка Swarm Bee..'; sleep 2
 date "+【%Y-%m-%d %H:%M:%S】 Установка Swarm Bee" 2>&1 | tee -a /root/run.log
-curl -s https://raw.githubusercontent.com/ethersphere/bee/master/install.sh | TAG=v0.5.0 bash
+curl -s https://raw.githubusercontent.com/ethersphere/bee/master/install.sh | TAG=v0.5.1 bash
 
 echo 'Установка Bee Clef..'; sleep 2
 
